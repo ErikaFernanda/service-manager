b6ba4b7d5730ee6c210bdd2f8a2d5a9ba74678d4
Erika Fernanda <erika.fernanda@smartsuite.imd.ufrn.br>
2023-10-30 19:48:48 -0300

    SM-2: Remover arquivos .pyc de versionamenti

--- service_manager/service_manager_api/settings.py
+++ service_manager/service_manager_api/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': 5432,
         'CONN_MAX_AGE': 500,
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 # Password validation
